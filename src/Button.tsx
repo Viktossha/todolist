38e54ebab0f0848427edc5690e3d4b0b12531dec
user <vikapetrova8@gmail.com>
2024-01-23 00:18:42 +0300

    btn fix

--- src/Button.tsx
+++ src/Button.tsx
@@ -3,10 +3,11 @@ import React from 'react';
 type ButtonPropsType = {
     title: string
     onClickHandler: () => void
+    isDisabled?: boolean
     class?: string
 }
-export const Button: React.FC<ButtonPropsType> = ({title, onClickHandler, ...props}) => {
+export const Button: React.FC<ButtonPropsType> = ({title, onClickHandler, isDisabled, ...props}) => {
     return (
-        <button className={props.class} onClick={onClickHandler}>{title}</button>
+        <button className={props.class} disabled={isDisabled} onClick={onClickHandler}>{title}</button>
     );
 };

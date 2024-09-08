340673cca55e563a17a6ce3ee7ce6d652fa6b918
Vika <vikapetrova8@gmail.com>
2024-09-08 16:40:22 +0300

    delete and update files

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,26 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
-import {TaskType, Todolist} from "./Todolist";
-import {v1} from "uuid";
-
-export type FilterValuesType = 'all' | 'active' | 'completed';
 
 function App() {
-
-    //let arr = useState(initTasks);
-    // let tasks = arr[0];
-    // let setTasks = arr[1];
-
-    //короткая запись
-    //let [tasks, setTasks] = arr;
-
-    //еще короче
-    let [tasks, setTasks] = useState<Array<TaskType>>([
-        {id: v1(), title: 'HTML&CSS', isDone: true},
-        {id: v1(), title: 'JS', isDone: true},
-        {id: v1(), title: 'React', isDone: false},
-    ]);
-
-    let [filter, setFilter] = useState<FilterValuesType>('all');
-
-    function removeTask(id: string) {
-        let filteredTasks: Array<TaskType> = tasks.filter(t => t.id !== id);
-        setTasks(filteredTasks);
-    }
-
-    function addTask(title: string) {
-        let newTask = {id: v1(), title: title, isDone: false};
-        let newTasks = [newTask, ...tasks];
-        setTasks(newTasks);
-    }
-
-    function changeStatus(taskId: string, isDone: boolean) {
-        let task = tasks.find((t) => t.id === taskId);
-        if (task) {
-            task.isDone = isDone;
-        }
-        setTasks([...tasks])
-    }
-
-    function changeFilter(value: FilterValuesType) {
-        setFilter(value)
-    }
-
-    let tasksForTodoList = tasks;
-    if (filter === 'completed') {
-        tasksForTodoList = tasks.filter(t => t.isDone)
-    }
-    if (filter === 'active') {
-        tasksForTodoList = tasks.filter(t => !t.isDone)
-    }
-
     return (
         <div className="App">
-            <Todolist title='What to learn' tasks={tasksForTodoList} removeTask={removeTask} changeFilter={changeFilter} addTask={addTask} changeStatus={changeStatus} filter={filter}/>
+            <div>
+                <h3>What to learn</h3>
+                <div>
+                    <input/>
+                    <button>+</button>
+                </div>
+                <ul>
+                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
+                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
+                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
+                </ul>
+                <div>
+                    <button>All</button>
+                    <button>Active</button>
+                    <button>Completed</button>
+                </div>
+            </div>
         </div>
     );
 }

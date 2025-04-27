b6b59c2de85074e90d439e1799e08ff1fef9e36f
Vika <vikapetrova8@gmail.com>
2025-04-27 13:59:51 +0300

    todolistsApi

---
+++ src/features/todolists/api/todolistsApi.ts
@@ -0,0 +1,20 @@
+import type { Todolist } from "./todolistsApi.types"
+import { instanse } from "common/instance/instanse"
+import type { Response } from "common/types"
+
+export const todolistsApi = {
+  getTodolist: () => {
+    return instanse.get<Todolist[]>("todo-lists")
+  },
+  createTodolist: (title: string) => {
+    return instanse.post<Response<{ item: Todolist }>>("todo-lists", { title })
+  },
+  deleteTodolist: (id: string) => {
+    return instanse.delete<Response>(`todo-lists/${id}`)
+  },
+  updateTodolist: (args: { id: string; title: string }) => {
+    return instanse.put<Response>(`todo-lists/${args.id}`, {
+      title: args.title,
+    })
+  },
+}

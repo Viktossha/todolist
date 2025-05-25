55820650041c537059d2af14c5052bf2261d320b
Vika <vikapetrova8@gmail.com>
2025-05-25 17:33:31 +0300

    fix todolists-tasks-reducer tests

--- src/features/todolists/model/__tests__/todolists-tasks-reducer.test.ts
+++ src/features/todolists/model/__tests__/todolists-tasks-reducer.test.ts
@@ -1,12 +1,13 @@
 import { addTodoListAC, type DomainTodolist, todolistsReducer } from "../todolists-reducer"
 import { tasksReducer } from "../tasks-reducer"
 import type { TasksType } from "../../../../app/App"
+import { v1 } from "uuid"
 
 test("ids should be equals", () => {
   const startTasksState: TasksType = {}
   const startTodolistsState: DomainTodolist[] = []
 
-  const action = addTodoListAC("new todolist")
+  const action = addTodoListAC({ id: v1(), title: "new todolist", order: 0, addedDate: "" })
 
   const endTasksState = tasksReducer(startTasksState, action)
   const endTodolistsState = todolistsReducer(startTodolistsState, action)
@@ -15,6 +16,6 @@ test("ids should be equals", () => {
   const idFromTasks = keys[0]
   const idFromTodolists = endTodolistsState[0].id
 
-  expect(idFromTasks).toBe(action.payload.todolistId)
-  expect(idFromTodolists).toBe(action.payload.todolistId)
+  expect(idFromTasks).toBe(action.payload.todolist.id)
+  expect(idFromTodolists).toBe(action.payload.todolist.id)
 })

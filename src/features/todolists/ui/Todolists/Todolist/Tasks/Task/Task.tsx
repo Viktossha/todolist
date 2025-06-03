df35dff9bb1bd4562d926efc0224c04f0f30494d
Vika <vikapetrova8@gmail.com>
2025-06-03 22:35:25 +0300

    add disabled prop to elements of Task

--- src/features/todolists/ui/Todolists/Todolist/Tasks/Task/Task.tsx
+++ src/features/todolists/ui/Todolists/Todolist/Tasks/Task/Task.tsx
@@ -43,11 +43,19 @@ export const Task = React.memo(({ task, todolist }: TaskPropsType) => {
   return (
     <ListItem sx={getListItemSX(task.status === TaskStatus.Completed)}>
       <div>
-        <Checkbox onChange={changeTaskStatus} checked={task.status === TaskStatus.Completed} />
-        <EditableSpan oldTitle={task.title} updateTitle={updateTaskTitle} />
+        <Checkbox
+          onChange={changeTaskStatus}
+          checked={task.status === TaskStatus.Completed}
+          disabled={todolist.entityStatus === "loading"}
+        />
+        <EditableSpan
+          oldTitle={task.title}
+          updateTitle={updateTaskTitle}
+          disabled={todolist.entityStatus === "loading"}
+        />
       </div>
 
-      <IconButton aria-label="delete" onClick={removeTask}>
+      <IconButton aria-label="delete" onClick={removeTask} disabled={todolist.entityStatus === "loading"}>
         <DeleteIcon />
       </IconButton>
     </ListItem>

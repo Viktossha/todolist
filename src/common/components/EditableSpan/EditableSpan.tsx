1f2221c93470daee475a1e77964e539c582c9320
Vika <vikapetrova8@gmail.com>
2025-06-03 22:29:11 +0300

    add disabled prop to EditableSpan

--- src/common/components/EditableSpan/EditableSpan.tsx
+++ src/common/components/EditableSpan/EditableSpan.tsx
@@ -1,43 +1,44 @@
-import React, {ChangeEvent, useState} from 'react';
-import TextField from "@mui/material/TextField";
+import React, { ChangeEvent, useState } from "react"
+import TextField from "@mui/material/TextField"
 
 type EditableSpanPropsType = {
-    oldTitle: string
-    updateTitle: (newTitle: string) => void
-    className?: string
+  oldTitle: string
+  updateTitle: (newTitle: string) => void
+  className?: string
+  disabled?: boolean
 }
 
-export const EditableSpan = React.memo(({oldTitle, className, updateTitle}: EditableSpanPropsType) => {
-    //console.log('EditableSpan')
-    const [edit, setEdit] = useState(false)
-    const [newTitle, setNewTitle] = useState(oldTitle)
+export const EditableSpan = React.memo(({ oldTitle, className, updateTitle, disabled }: EditableSpanPropsType) => {
+  //console.log('EditableSpan')
+  const [edit, setEdit] = useState(false)
+  const [newTitle, setNewTitle] = useState(oldTitle)
 
-    const editModeHandler = () => {
-        setEdit(!edit)
-        if (edit) updateTitleHandler()
-    }
+  const editModeHandler = () => {
+    if (disabled) return
+    setEdit(!edit)
+    if (edit) updateTitleHandler()
+  }
 
+  const changeTaskTitleHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setNewTitle(e.currentTarget.value)
+  }
 
-    const changeTaskTitleHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        setNewTitle(e.currentTarget.value)
-    }
+  const updateTitleHandler = () => {
+    updateTitle(newTitle)
+  }
 
-    const updateTitleHandler = () => {
-        updateTitle(newTitle)
-    }
-
-    return (
-        edit
-             ?
-            <TextField
-                variant={'outlined'}
-                value={newTitle}
-                size={'small'}
-                autoFocus
-                onChange={changeTaskTitleHandler}
-                onBlur={editModeHandler}
-            />
-            : <span className={className} onDoubleClick={editModeHandler}>{oldTitle}</span>
-    )
-        ;
-});
+  return edit ? (
+    <TextField
+      variant={"outlined"}
+      value={newTitle}
+      size={"small"}
+      autoFocus
+      onChange={changeTaskTitleHandler}
+      onBlur={editModeHandler}
+    />
+  ) : (
+    <span className={className} onDoubleClick={editModeHandler}>
+      {oldTitle}
+    </span>
+  )
+})
--- src/features/todolists/ui/Todolists/Todolist/TodoListHeader/TodoListHeader.tsx
+++ src/features/todolists/ui/Todolists/Todolist/TodoListHeader/TodoListHeader.tsx
@@ -2,12 +2,7 @@ import React, { useCallback } from "react"
 import { EditableSpan } from "common/components"
 import IconButton from "@mui/material/IconButton"
 import DeleteIcon from "@mui/icons-material/Delete"
-import {
-  type DomainTodolist,
-  removeTodolistTC,
-  updateTodoListTitleAC,
-  updateTodolistTitleTC,
-} from "../../../../model/todolists-reducer"
+import { type DomainTodolist, removeTodolistTC, updateTodolistTitleTC } from "../../../../model/todolists-reducer"
 import { useAppDispatch } from "../../../../../../app/hooks"
 import styles from "./TodoListHeader.module.css"
 
@@ -30,7 +25,12 @@ export const TodoListHeader = ({ todolist }: TodoListHeaderPropsType) => {
 
   return (
     <div>
-      <EditableSpan className={styles.todolistTitle} oldTitle={todolist.title} updateTitle={updateTodoListTitle} />
+      <EditableSpan
+        className={styles.todolistTitle}
+        oldTitle={todolist.title}
+        updateTitle={updateTodoListTitle}
+        disabled={todolist.entityStatus === "loading"}
+      />
       <IconButton aria-label="delete" onClick={removeTodoList} disabled={todolist.entityStatus === "loading"}>
         <DeleteIcon />
       </IconButton>

64d36c8a7a17397ead7c63604357769a2f7ff550
Vika <vikapetrova8@gmail.com>
2025-06-12 13:45:50 +0300

    add Routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,20 +4,14 @@ import { ThemeProvider } from "@mui/material/styles"
 import CssBaseline from "@mui/material/CssBaseline"
 import { getTheme } from "common/theme"
 import { ErrorSnackbar, Header } from "common/components"
-import { Main } from "./Main"
 import { useAppSelector } from "./hooks"
 import { selectThemeMode } from "./app-selectors"
 import type { DomainTask } from "../features/todolists/api/tasksApi.types"
+import { Routing } from "common/routing"
 
 type ThemeMode = "dark" | "light"
 
 export type FilterValuesType = "all" | "active" | "completed"
-
-// export type TodoListType = {
-//   id: string
-//   title: string
-//   filter: FilterValuesType
-// }
 
 export type TasksType = {
   [key: string]: DomainTask[]
@@ -32,7 +26,7 @@ function App() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Header />
-      <Main />
+      <Routing />
       <ErrorSnackbar />
     </ThemeProvider>
   )
--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,14 @@ import "./index.css"
 import App from "./app/App"
 import { Provider } from "react-redux"
 import { store } from "./app/store"
+import { BrowserRouter } from "react-router"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
 )
 

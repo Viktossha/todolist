522cea8ac9895f62ccc8df2af5e71d7d5bb29295
Vika <vikapetrova8@gmail.com>
2025-06-12 23:17:55 +0300

    add check for isLoggedIn

--- src/app/Main.tsx
+++ src/app/Main.tsx
@@ -1,12 +1,17 @@
-import React, { useCallback } from "react"
+import React, { useCallback, useEffect } from "react"
 import Container from "@mui/material/Container"
 import Grid from "@mui/material/Grid"
 import { AddItemForm } from "common/components"
 import { Todolists } from "../features/todolists/ui/Todolists/Todolists"
 import { addTodoListAC, addTodolistTC } from "../features/todolists/model/todolists-reducer"
-import { useAppDispatch } from "./hooks"
+import { useAppDispatch, useAppSelector } from "./hooks"
+import { useNavigate } from "react-router"
+import { Path } from "common/routing"
+import { selectIsLoggedIn } from "../features/auth/model/auth-selector"
 
 export const Main = () => {
+  const isLoggedIn = useAppSelector(selectIsLoggedIn)
+
   let dispatch = useAppDispatch()
 
   const addTodoList = useCallback(
@@ -15,6 +20,13 @@ export const Main = () => {
     },
     [dispatch],
   )
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      navigate(Path.Login)
+    }
+  }, [isLoggedIn])
 
   return (
     <Container fixed>

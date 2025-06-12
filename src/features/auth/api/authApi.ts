5426fd5b3b39e836ad3dfdf926d4029dfce5fed1
Vika <vikapetrova8@gmail.com>
2025-06-12 23:18:39 +0300

    add logout and me api

--- src/features/auth/api/authApi.ts
+++ src/features/auth/api/authApi.ts
@@ -6,4 +6,10 @@ export const authApi = {
   login(args: LoginArgs) {
     return instance.post<Response<{ userId: number; token: string }>>("auth/login", args)
   },
+  logout() {
+    return instance.delete<Response>("auth/login")
+  },
+  me() {
+    return instance.get<Response<{ id: number; email: string; login: string }>>("auth/me")
+  },
 }

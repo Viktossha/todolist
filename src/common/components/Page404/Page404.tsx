845677309bdb3d89e4cfcb0f8d47a2226e14e306
Vika <vikapetrova8@gmail.com>
2025-06-13 20:25:58 +0300

    add link "To the main page"

--- src/common/components/Page404/Page404.tsx
+++ src/common/components/Page404/Page404.tsx
@@ -1,11 +1,16 @@
 import React from "react"
 import s from "./Page404.module.css"
+import Button from "@mui/material/Button"
+import { Link } from "react-router"
 
 export const Page404 = () => {
   return (
-    <>
+    <div className={s.mainBlock}>
       <h1 className={s.title}>404</h1>
       <h2 className={s.subTitle}>page not found</h2>
-    </>
+      <Button component={Link} to={"/"} variant={"outlined"}>
+        To the main page
+      </Button>
+    </div>
   )
 }

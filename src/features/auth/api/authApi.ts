cfd0eaefd0c92df9575ae29a8aa767999da85b4f
Vika <vikapetrova8@gmail.com>
2025-07-24 20:45:33 +0300

    add RTK query authApi

--- src/features/auth/api/authApi.ts
+++ src/features/auth/api/authApi.ts
@@ -1,8 +1,38 @@
 import type { Response } from "common/types"
 import type { LoginArgs } from "./authApi.types"
 import { instance } from "common/instance"
+import { baseApi } from "../../../app/baseApi"
 
-export const authApi = {
+export const authApi = baseApi.injectEndpoints({
+  endpoints: (builder) => {
+    return {
+      me: builder.query<Response<{ id: number; email: string; login: string }>, void>({
+        query: () => "auth/me",
+      }),
+      login: builder.mutation<Response<{ userId: number; token: string }>, LoginArgs>({
+        query: (args) => {
+          return {
+            method: "POST",
+            url: "auth/login",
+            body: args,
+          }
+        },
+      }),
+      logout: builder.mutation<Response, void>({
+        query: () => {
+          return {
+            method: "DELETE",
+            url: "auth/login",
+          }
+        },
+      }),
+    }
+  },
+})
+
+export const { useMeQuery, useLogoutMutation, useLoginMutation } = authApi
+
+export const _authApi = {
   login(args: LoginArgs) {
     return instance.post<Response<{ userId: number; token: string }>>("auth/login", args)
   },

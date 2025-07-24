247a3738a6e97c513b582298de3a35308a37fbb7
Vika <vikapetrova8@gmail.com>
2025-07-24 20:46:42 +0300

    refactor query me

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import "./App.css"
 import { ThemeProvider } from "@mui/material/styles"
 import CssBaseline from "@mui/material/CssBaseline"
@@ -6,10 +6,12 @@ import { getTheme } from "common/theme"
 import { ErrorSnackbar, Header } from "common/components"
 import { useAppDispatch, useAppSelector } from "./hooks"
 import { Routing } from "common/routing"
-import { initializeAppTC, selectIsInitialized } from "../features/auth/model/authSlice"
+import { setIsLoggedIn } from "../features/auth/model/authSlice"
 import { CircularProgress } from "@mui/material"
 import s from "./App.module.css"
 import { selectThemeMode } from "./appSlice"
+import { useMeQuery } from "../features/auth/api/authApi"
+import { ResultCode } from "common/enums/enums"
 
 type ThemeMode = "dark" | "light"
 
@@ -17,14 +19,22 @@ export type FilterValuesType = "all" | "active" | "completed"
 
 function App() {
   let themeMode = useAppSelector(selectThemeMode)
-  const isInitialized = useAppSelector(selectIsInitialized)
+  const dispatch = useAppDispatch()
+
+  const [isInitialized, setIsInitialized] = useState(false)
+
+  const { data, isLoading } = useMeQuery()
 
   const theme = getTheme(themeMode)
-  let dispatch = useAppDispatch()
 
   useEffect(() => {
-    dispatch(initializeAppTC())
-  }, [])
+    if (!isLoading) {
+      setIsInitialized(true)
+      if (data?.resultCode === ResultCode.Success) {
+        dispatch(setIsLoggedIn({ isLoggedIn: true }))
+      }
+    }
+  }, [isLoading, data])
 
   if (!isInitialized) {
     return (
--- src/features/auth/model/authSlice.ts
+++ src/features/auth/model/authSlice.ts
@@ -1,6 +1,6 @@
 import type { LoginArgs } from "../api/authApi.types"
 import type { AppDispatch } from "../../../app/store"
-import { authApi } from "../api/authApi"
+import { _authApi } from "../api/authApi"
 import { ResultCode } from "common/enums/enums"
 import { handleServerAppError, handleServerNetworkError } from "common/utils"
 import { clearTodolist } from "../../todolists/model/todolistsSlice"
@@ -12,7 +12,6 @@ export const authSlice = createSlice({
   name: "auth",
   initialState: {
     isLoggedIn: false,
-    isInitialized: false,
   },
   // reducers: {
   //   setIsLoggedIn: (state, action: PayloadAction<{ isLoggedIn: boolean }>) => {
@@ -29,25 +28,21 @@ export const authSlice = createSlice({
       setIsLoggedIn: create.reducer<{ isLoggedIn: boolean }>((state, action) => {
         state.isLoggedIn = action.payload.isLoggedIn
       }),
-      setIsInitialized: create.reducer<{ isInitialized: boolean }>((state, action) => {
-        state.isInitialized = action.payload.isInitialized
-      }),
     }
   },
   selectors: {
     selectIsLoggedIn: (state) => state.isLoggedIn,
-    selectIsInitialized: (state) => state.isInitialized,
   },
 })
 
 export const authReducer = authSlice.reducer
-export const { setIsLoggedIn, setIsInitialized } = authSlice.actions
-export const { selectIsLoggedIn, selectIsInitialized } = authSlice.selectors
+export const { setIsLoggedIn } = authSlice.actions
+export const { selectIsLoggedIn } = authSlice.selectors
 
 // thunks
 export const loginTC = (data: LoginArgs) => (dispatch: AppDispatch) => {
   dispatch(setAppStatus({ status: "loading" }))
-  authApi
+  _authApi
     .login(data)
     .then((res) => {
       if (res.data.resultCode === ResultCode.Success) {
@@ -65,7 +60,7 @@ export const loginTC = (data: LoginArgs) => (dispatch: AppDispatch) => {
 
 export const logoutTC = () => (dispatch: AppDispatch) => {
   dispatch(setAppStatus({ status: "loading" }))
-  authApi
+  _authApi
     .logout()
     .then((res) => {
       if (res.data.resultCode === ResultCode.Success) {
@@ -85,7 +80,7 @@ export const logoutTC = () => (dispatch: AppDispatch) => {
 
 export const initializeAppTC = () => (dispatch: AppDispatch) => {
   dispatch(setAppStatus({ status: "loading" }))
-  authApi
+  _authApi
     .me()
     .then((res) => {
       if (res.data.resultCode === ResultCode.Success) {
@@ -98,5 +93,4 @@ export const initializeAppTC = () => (dispatch: AppDispatch) => {
     .catch((err) => {
       handleServerNetworkError(err, dispatch)
     })
-    .finally(() => dispatch(setIsInitialized({ isInitialized: true })))
 }

d631f9b5546f1fab6dc8b27132ffe590497dcb0f
Vika <vikapetrova8@gmail.com>
2025-07-30 00:02:46 +0300

    move isLoggedIn to appSlice

--- src/app/appSlice.ts
+++ src/app/appSlice.ts
@@ -1,4 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit"
+import type { LoginArgs } from "../features/auth/api/authApi.types"
+import type { AppDispatch } from "./store"
+import { _authApi } from "../features/auth/api/authApi"
+import { ResultCode } from "common/enums/enums"
+import { handleServerAppError, handleServerNetworkError } from "common/utils"
+import { clearTasks } from "../features/todolists/model/tasksSlice"
+import { clearTodolist } from "../features/todolists/model/todolistsSlice"
 
 export type ThemeMode = "dark" | "light"
 export type RequestStatus = "idle" | "loading" | "succeeded" | "failed"
@@ -9,6 +16,7 @@ export const appSlice = createSlice({
     themeMode: "light" as ThemeMode,
     status: "idle" as RequestStatus,
     error: null as string | null,
+    isLoggedIn: false,
   },
   reducers: (create) => {
     return {
@@ -21,16 +29,58 @@ export const appSlice = createSlice({
       setAppError: create.reducer<{ error: string | null }>((state, action) => {
         state.error = action.payload.error
       }),
+      setIsLoggedIn: create.reducer<{ isLoggedIn: boolean }>((state, action) => {
+        state.isLoggedIn = action.payload.isLoggedIn
+      }),
     }
   },
   selectors: {
     selectThemeMode: (state) => state.themeMode,
     selectAppStatus: (state) => state.status,
     selectAppError: (state) => state.error,
+    selectIsLoggedIn: (state) => state.isLoggedIn,
   },
 })
 
 export const appReducer = appSlice.reducer
-export const { setAppStatus, setAppError, changeTheme } = appSlice.actions
-export const { selectThemeMode, selectAppStatus, selectAppError } = appSlice.selectors
+export const { setAppStatus, setAppError, changeTheme, setIsLoggedIn } = appSlice.actions
+export const { selectThemeMode, selectAppStatus, selectAppError, selectIsLoggedIn } = appSlice.selectors
 export type AppInitialState = ReturnType<typeof appSlice.getInitialState>
+
+export const loginTC = (data: LoginArgs) => (dispatch: AppDispatch) => {
+  dispatch(setAppStatus({ status: "loading" }))
+  _authApi
+    .login(data)
+    .then((res) => {
+      if (res.data.resultCode === ResultCode.Success) {
+        dispatch(setIsLoggedIn({ isLoggedIn: true }))
+        dispatch(setAppStatus({ status: "succeeded" }))
+        localStorage.setItem("sn-token", res.data.data.token)
+      } else {
+        handleServerAppError(dispatch, res.data)
+      }
+    })
+    .catch((err) => {
+      handleServerNetworkError(err, dispatch)
+    })
+}
+
+export const logoutTC = () => (dispatch: AppDispatch) => {
+  dispatch(setAppStatus({ status: "loading" }))
+  _authApi
+    .logout()
+    .then((res) => {
+      if (res.data.resultCode === ResultCode.Success) {
+        dispatch(setIsLoggedIn({ isLoggedIn: false }))
+        dispatch(setAppStatus({ status: "succeeded" }))
+        localStorage.removeItem("sn-token")
+        dispatch(clearTasks())
+        dispatch(clearTodolist())
+      } else {
+        handleServerAppError(dispatch, res.data)
+      }
+    })
+    .catch((err) => {
+      handleServerNetworkError(err, dispatch)
+    })
+}

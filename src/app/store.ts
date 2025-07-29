ec42a0003de015f44f88a777a74de9e999faa40c
Vika <vikapetrova8@gmail.com>
2025-07-30 00:21:02 +0300

    remove authSlice

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,7 +3,6 @@ import { tasksReducer, tasksSlice } from "../features/todolists/model/tasksSlice
 import { todolistsReducer, todolistsSlice } from "../features/todolists/model/todolistsSlice"
 import { appReducer, appSlice } from "./appSlice"
 import { type ThunkDispatch } from "redux-thunk"
-import { authReducer, authSlice } from "../features/auth/model/authSlice"
 import { configureStore } from "@reduxjs/toolkit"
 import { setupListeners } from "@reduxjs/toolkit/query"
 import { baseApi } from "./baseApi"
@@ -14,7 +13,6 @@ export const store = configureStore({
     [tasksSlice.name]: tasksReducer,
     [todolistsSlice.name]: todolistsReducer,
     [appSlice.name]: appReducer,
-    [authSlice.name]: authReducer,
     [baseApi.reducerPath]: baseApi.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(baseApi.middleware),

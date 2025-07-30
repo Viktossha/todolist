e2b1a27f49ee078fb93851cb4ea640ec480377dc
Vika <vikapetrova8@gmail.com>
2025-07-30 22:59:43 +0300

    add RTK query tasksApi

--- src/features/todolists/api/tasksApi.ts
+++ src/features/todolists/api/tasksApi.ts
@@ -1,8 +1,51 @@
 import type { DomainTask, GetTasksResponse, UpdateTaskModel } from "./tasksApi.types"
 import type { Response } from "common/types"
 import { instance } from "common/instance"
+import { baseApi } from "../../../app/baseApi"
 
-export const tasksApi = {
+export const tasksApi = baseApi.injectEndpoints({
+  endpoints: (builder) => {
+    return {
+      getTasks: builder.query<GetTasksResponse, string>({
+        query: (todolistId) => `todo-lists/${todolistId}/tasks`,
+        providesTags: ["Task"],
+      }),
+      createTask: builder.mutation<Response<{ item: DomainTask }>, { title: string; todolistId: string }>({
+        query: ({ title, todolistId }) => {
+          return {
+            method: "POST",
+            url: `todo-lists/${todolistId}/tasks`,
+            body: { title },
+          }
+        },
+      }),
+      deleteTask: builder.mutation<Response, { todolistId: string; taskId: string }>({
+        query: ({ todolistId, taskId }) => {
+          return {
+            method: "DELETE",
+            url: `todo-lists/${todolistId}/tasks/${taskId}`,
+          }
+        },
+      }),
+      updateTask: builder.mutation<
+        Response<{ item: DomainTask }>,
+        { todolistId: string; taskId: string; model: UpdateTaskModel }
+      >({
+        query: ({ todolistId, taskId, model }) => {
+          return {
+            method: "PUT",
+            url: `todo-lists/${todolistId}/tasks/${taskId}`,
+            body: model,
+          }
+        },
+      }),
+    }
+  },
+})
+
+export const { useGetTasksQuery, useCreateTaskMutation, useDeleteTaskMutation, useUpdateTaskMutation } = tasksApi
+
+export const _tasksApi = {
   getTasks(todolistId: string) {
     return instance.get<GetTasksResponse>(`todo-lists/${todolistId}/tasks`)
   },

266e01411ce3293a7e62a5f7b11cc82806cac465
user <vikapetrova8@gmail.com>
2025-08-05 18:16:54 +0300

    refactor createTask

--- src/features/todolists/ui/Todolists/Todolist/TodoList.tsx
+++ src/features/todolists/ui/Todolists/Todolist/TodoList.tsx
@@ -1,12 +1,10 @@
-import React, { useCallback } from "react"
+import React from "react"
 // import {Button} from "./Button";
-import { TodoListHeader } from "./TodoListHeader/TodoListHeader"
-import { AddItemForm } from "common/components"
-import { Tasks } from "./Tasks/Tasks"
-import { FilterTasksButtons } from "./FilterTasksButtons/FilterTasksButtons"
-import { addTaskTC } from "../../../model/tasksSlice"
-import { useAppDispatch } from "../../../../../app/hooks"
-import type { DomainTodolist } from "../../../model/todolistsSlice"
+import {TodoListHeader} from "./TodoListHeader/TodoListHeader"
+import {AddItemForm} from "common/components"
+import {Tasks} from "./Tasks/Tasks"
+import {FilterTasksButtons} from "./FilterTasksButtons/FilterTasksButtons"
+import type {DomainTodolist} from "../../../model/todolistsSlice"
 import {useCreateTaskMutation} from "../../../api/tasksApi";
 
 type TodoListPropsType = {

b9f161cafdd65df375e703b39f982790a345ba16
user <vikapetrova8@gmail.com>
2025-08-18 15:05:08 +0300

    refactor updateTodolistEntityStatus

--- src/features/todolists/ui/Todolists/Todolist/TodoListHeader/TodoListHeader.tsx
+++ src/features/todolists/ui/Todolists/Todolist/TodoListHeader/TodoListHeader.tsx
@@ -2,23 +2,39 @@ import React, { useCallback } from "react"
 import { EditableSpan } from "common/components"
 import IconButton from "@mui/material/IconButton"
 import DeleteIcon from "@mui/icons-material/Delete"
-import { type DomainTodolist, removeTodolistTC, updateTodolistTitleTC } from "../../../../model/todolistsSlice"
+import { type DomainTodolist } from "../../../../model/todolistsSlice"
 import { useAppDispatch } from "../../../../../../app/hooks"
 import styles from "./TodoListHeader.module.css"
-import { useDeleteTodolistMutation, useUpdateTodolistMutation } from "../../../../api/todolistsApi"
+import { todolistsApi, useDeleteTodolistMutation, useUpdateTodolistMutation } from "../../../../api/todolistsApi"
+import type { RequestStatus } from "../../../../../../app/appSlice"
 
 type TodoListHeaderPropsType = {
   todolist: DomainTodolist
 }
 export const TodoListHeader = ({ todolist }: TodoListHeaderPropsType) => {
+  const dispatch = useAppDispatch()
   const [updateTodolist] = useUpdateTodolistMutation()
   const updateTodoListTitle = (newTitle: string) => {
     updateTodolist({ id: todolist.id, title: newTitle })
   }
 
   const [deleteTodolist] = useDeleteTodolistMutation()
+
+  const updateQueryData = (status: RequestStatus) => {
+    dispatch(
+      todolistsApi.util.updateQueryData("getTodolists", undefined, (state) => {
+        const todo = state.find((todo) => todo.id === todolist.id)
+        if (todo) todo.entityStatus = status
+      }),
+    )
+  }
   const removeTodoList = () => {
+    updateQueryData("loading")
     deleteTodolist(todolist.id)
+      .unwrap()
+      .catch(() => {
+        updateQueryData("failed")
+      })
   }
 
   return (

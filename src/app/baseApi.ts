5526b4a1ab559aa7f124961911c407c00ea24e85
user <vikapetrova8@gmail.com>
2025-08-19 20:01:17 +0300

    add delay emulation

--- src/app/baseApi.ts
+++ src/app/baseApi.ts
@@ -2,13 +2,16 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
 export const baseApi = createApi({
   reducerPath: "todolistsApi",
-  baseQuery: fetchBaseQuery({
-    baseUrl: process.env.REACT_APP_BASE_URL,
-    prepareHeaders: (headers) => {
-      headers.set("API-KEY", `${process.env.REACT_APP_API_KEY}`)
-      headers.set("Authorization", `Bearer ${localStorage.getItem("sn-token")}`)
-    },
-  }),
+  baseQuery: async (args, api, extraOptions) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000)) //эмуляция задержки
+    return fetchBaseQuery({
+      baseUrl: process.env.REACT_APP_BASE_URL,
+      prepareHeaders: (headers) => {
+        headers.set("API-KEY", `${process.env.REACT_APP_API_KEY}`)
+        headers.set("Authorization", `Bearer ${localStorage.getItem("sn-token")}`)
+      },
+    })(args, api, extraOptions)
+  },
   tagTypes: ["Todolist", "Task"],
   endpoints: () => ({}),
 })

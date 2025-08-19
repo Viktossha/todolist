983e7384be4ad8753eccb64f9c93c5d4298d7583
user <vikapetrova8@gmail.com>
2025-08-19 21:08:14 +0300

    add error handling

--- src/app/baseApi.ts
+++ src/app/baseApi.ts
@@ -1,16 +1,33 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+import { setAppError } from "./appSlice"
 
 export const baseApi = createApi({
   reducerPath: "todolistsApi",
   baseQuery: async (args, api, extraOptions) => {
-    await new Promise((resolve) => setTimeout(resolve, 2000)) //эмуляция задержки
-    return fetchBaseQuery({
+    //await new Promise((resolve) => setTimeout(resolve, 2000)) //эмуляция задержки
+    const result = await fetchBaseQuery({
       baseUrl: process.env.REACT_APP_BASE_URL,
       prepareHeaders: (headers) => {
         headers.set("API-KEY", `${process.env.REACT_APP_API_KEY}`)
         headers.set("Authorization", `Bearer ${localStorage.getItem("sn-token")}`)
       },
     })(args, api, extraOptions)
+
+    if (result.error) {
+      if (result.error.status === "FETCH_ERROR" || result.error.status === "PARSING_ERROR") {
+        api.dispatch(setAppError({ error: result.error.error }))
+      }
+
+      if (result.error.status === 400 || result.error.status === 500) {
+        api.dispatch(setAppError({ error: (result.error.data as { message: string }).message }))
+      }
+
+      if (result.error.status === 403) {
+        api.dispatch(setAppError({ error: "403 Forbidden Error. Check API-KEY" }))
+      }
+    }
+
+    return result
   },
   tagTypes: ["Todolist", "Task"],
   endpoints: () => ({}),
